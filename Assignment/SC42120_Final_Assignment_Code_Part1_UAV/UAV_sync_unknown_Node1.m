ade84ff49d1d8d7a635b211e0a2e26246692fda2
Abishekvupputur <abishekvupputur@gmail.com>
2019-03-26 12:04:49 +0100

    NODE1_NODE3

--- Assignment/SC42120_Final_Assignment_Code_Part1_UAV/UAV_sync_unknown_Node1.m
+++ Assignment/SC42120_Final_Assignment_Code_Part1_UAV/UAV_sync_unknown_Node1.m
@@ -7,10 +7,16 @@ init_eul1 = [0.5 0.05 0.5];
 init_vel1 = [25 0 0];
 init_ang1 = [0 0 0];
 I1 = [0.1 0 -0.01;0 0.05 0;-0.01 0 0.1];
+M3 = 40;
+init_pos3 = [-50 250 -150];
+init_eul3 = [-0.5 -0.05 -0.5];
+init_vel3 = [20 0 0];
+init_ang3 = [0 0 0];
+I3 = [0.4 0 -0.04;0 0.2 0;-0.04 0 0.4];
 
 
 Am=[zeros(6),eye(6);-Kp,-Kv];
-Bm=[zeros(6,6);ones(6,6)];
+Bm=[zeros(6,6);eye(6,6)];
 Cm=[eye(12)];
 Dm=zeros(12,6);
 Pm=lyap(Am',100*eye(12));
